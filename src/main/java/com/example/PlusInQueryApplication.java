74915fe7688491b8b17ae4bbd02dec832391efde
Matt Sicker <matthew.sicker@ahold.com>
2016-05-02 09:07:51 -0500

    Initial commit
    This repo is for demonstrating a bug in rest dsl.

---
+++ src/main/java/com/example/PlusInQueryApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusInQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlusInQueryApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/PlusInQueryRoute.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author msicker
+ */
+@Component
+public class PlusInQueryRoute extends RouteBuilder {
+	@Override
+	public void configure() throws Exception {
+		restConfiguration()
+			.component("netty4-http")
+			.contextPath("piq")
+			.bindingMode(RestBindingMode.json)
+			.host("0.0.0.0")
+			.port(8080);
+
+		rest()
+			.get("/test")
+				.route()
+					.setBody().header("foo")
+			.endRest();
+	}
+}
